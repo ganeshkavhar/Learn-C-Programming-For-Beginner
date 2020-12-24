e7764ff44381b69de5b743938c75f3d6f6fc779d
ganesh kavhar <kavharg@gmail.com>
2020-12-24 19:02:23 +0530

    Create Arithmaticinc.c

---
+++ Arithmaticinc.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+   int op1, op2, sum;      // variable declaration
+   
+   op1 = 5;                // variable definition
+   op2 = 3;
+   
+   sum = op1 + op2;        // addition operation just change sign for other operation ex substraction , multiplication , division etc..
+   
+   printf("sum of %d and %d is %d", op1, op2, sum);
+}
