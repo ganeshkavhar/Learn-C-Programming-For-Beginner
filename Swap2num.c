7954651c623f1c08ead2d153d21dd6385797ebca
ganesh kavhar <kavharg@gmail.com>
2020-12-24 19:03:07 +0530

    Create Swap2num.c

---
+++ Swap2num.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+   int a, b;
+
+   a = 11;
+   b = 99;
+   
+   if(a > b)
+      printf("a is greater than b");
+   else
+      printf("a is not greater than b");
+
+   return 0;
+}
