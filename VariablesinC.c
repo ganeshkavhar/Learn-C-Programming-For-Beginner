65228b678c808ad56b075f6c87d100d1309ebbc0
ganesh kavhar <kavharg@gmail.com>
2020-12-24 19:01:16 +0530

    Create VariablesinC.c

---
+++ VariablesinC.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+   char c;        // char variable declaration
+   c = 'A';       // defining a char variable
+   
+   printf("value of c is %c", c);
+   
+   return 0;
+}
